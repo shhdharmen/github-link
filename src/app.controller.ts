16b18f9cc1dc490861f8b4f87e93ee21648f7389
Dharmen Shah <dharmen.s@milestoneinternet.com>
2021-03-03 17:17:00 +0530

    ci(vercel): add vercel.json

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Query, Res } from '@nestjs/common';
+import { Controller, Get, Header, Query, Res } from '@nestjs/common';
 import { Response } from 'express';
 import { map } from 'rxjs/operators';
 import { AppService } from './app.service';
@@ -9,6 +9,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @Header('Cache-Control', 's-max-age=1, stale-while-revalidate')
   getGhLink(@Query() queries: GhLinkQueries, @Res() res?: Response) {
     return this.appService.getGhLink(queries.ghUrl, queries.q).pipe(
       map((v) => {
